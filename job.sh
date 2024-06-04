830c9865363384976390f63154564aac6c691768
FattiMei <teomei68@gmail.com>
2024-06-04 18:11:32 +0200

    Add slurm job file

--- job.sh
+++ job.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=maxFib
-#SBATCH --output=maxFib.out
-#SBATCH --time=10:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=100M
+##SBATCH --job-name=matmul
+##SBATCH --output=matmul.out
+##SBATCH --time=00:10
+##SBATCH --ntasks=2
+##SBATCH --cpus-per-task=2
+##SBATCH --mem-per-cpu=100M
 
-## Add chmod +x job.sh && sbatch script.sh && squeue -u <username>
-
-srun singularity run main.sif
+srun singularity run --bind "$TMPDIR" ./main.sif 
