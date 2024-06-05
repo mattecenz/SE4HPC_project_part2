3f87d3b645c0f18fc900f04ecda82261bf50209a
FattiMei <teomei68@gmail.com>
2024-06-05 15:29:00 +0200

    Use the host mpi

--- job.sh
+++ job.sh
@@ -2,6 +2,7 @@
 
 #SBATCH --job-name=matmul
 #SBATCH --output=matmul.out
+#SBATCH --error=error.out
 #SBATCH --time=00:10
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
@@ -15,4 +16,4 @@ SINGULARITY_IMAGE=main.sif
 export TMPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-singularity exec --bind $TMPDIR:$TMPDIR $SINGULARITY_IMAGE bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /project/build/main"
+mpirun -np 2 singularity exec --bind $TMPDIR:$TMPDIR $SINGULARITY_IMAGE bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && /project/build/main"
